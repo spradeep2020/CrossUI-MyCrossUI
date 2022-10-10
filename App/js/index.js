89056b175862e3cac72bbb0628e1c815721341f5
spradeep2020 <55496160+spradeep2020@users.noreply.github.com>
2022-10-10 22:34:15 +0530

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,49 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label8")
+                .setRotate(358)
+                .setLeft("11.428571428571429em")
+                .setTop("4.571428571428571em")
+                .setWidth("9.295238095238096em")
+                .setCaption("DAQ  Dashboard")
+                .setCustomStyle({
+                    "KEY" : {
+                        "color" : "#008080",
+                        "background-color" : "#AFEEEE"
+                    }
+                })
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button8")
+                .setLeft("17.523809523809526em")
+                .setTop("9.142857142857142em")
+                .setCaption("ON")
+                .setCustomStyle({
+                    "KEY" : {
+                        "color" : "#32CD32"
+                    }
+                })
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button9")
+                .setLeft("28.952380952380953em")
+                .setTop("8.380952380952381em")
+                .setCaption("OFF")
+                .setCustomStyle({
+                    "KEY" : {
+                        "color" : "#FF4500"
+                    }
+                })
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
